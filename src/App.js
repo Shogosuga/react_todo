7e773b650ab4d8fa12217da180a1551dcb0611ac
Shogosuga <shogo1219tdj@gmail.com>
2019-12-14 23:52:11 +0900

    mod: add form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import TodoList from './TodoList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      todos : [
+        {
+          id : 1,
+          title : "Hello React!",
+          desc : "React start",
+          done :  false
+        },
+        {
+          id : 2,
+          title :  "Hello Wolrd!",
+          desc : "Start Redux",
+          done : false
+        },
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>
+          todo application
+        </h1>
+        <TodoList
+          todos={this.state.todos}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
