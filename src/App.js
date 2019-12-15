4ea46a43bad8006f8be5289a3f110468fb265663
Shogosuga <shogo1219tdj@gmail.com>
2019-12-15 11:58:11 +0900

    mod: add form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import TodoList from './TodoList';
+import Form from './Form'
 import './App.css';
 
 class App extends Component {
 
   constructor() {
     super()
-    this.state = {
-      todos : [
+    
+    const todos = [
         {
           id : 1,
           title : "Hello React!",
@@ -21,8 +22,35 @@ class App extends Component {
           done : false
         },
       ]
+    this.state = {
+      todos : todos,
+      countTodo : todos.length + 1,
     }
   }
+
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const desc = e.target.desc.value;
+    console.log(title)
+    const todos = this.state.todos.slice()
+    const countTodo = this.state.countTodo
+
+    todos.push({
+      id : countTodo,
+      title : title,
+      desc : desc,
+      done : false,
+    });
+
+    this.setState({ todos })
+    this.setState({ countTodo : countTodo + 1 })
+
+    e.target.title.value = '';
+    e.target.desc.value = '';
+  }
+
 
   render() {
     return (
@@ -30,6 +58,7 @@ class App extends Component {
         <h1>
           todo application
         </h1>
+        <Form handleSubmit={this.handleSubmit.bind(this)} />
         <TodoList
           todos={this.state.todos}
         />
